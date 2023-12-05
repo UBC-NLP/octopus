eab9106deb67bf065ec72bc8e5f893dc6a9630f3
AbdelRahim A. Elmadany <elmadany@gmail.com>
2023-12-04 16:09:20 -0800

    Update octopus.py

--- octopus/octopus.py
+++ octopus/octopus.py
@@ -61,12 +61,12 @@ class octopus():
         return validattion_results
     def do_generate(self, sources, search_method, seq_length=300, max_outputs=1, num_beams=5, no_repeat_ngram_size=2, top_p=0.95, top_k=50):
         device = ('cuda:0' if torch.cuda.is_available() else 'cpu')
-        encoding = self.tokenizer(sources,padding=True, return_tensors="pt")
+        encoding = self.tokenizer(sources,padding=True, return_tensors="pt").to(device)
         input_ids, attention_masks = encoding["input_ids"], encoding["attention_mask"]
         gen_kwargs = get_gen_kwargs(search_method, seq_length, max_outputs, num_beams, no_repeat_ngram_size, top_p, top_k, self.logger)
-        self.logger.info("Copying input to {}".format(device))
+        self.logger.info("** Copying input to {}".format(device))
         outputs = self.model.generate(
-                        input_ids=input_ids.to(device), 
+                        input_ids=input_ids, 
                         attention_mask=attention_masks,
                         **gen_kwargs,
                     )
